0593ff2252f94f6940387a804049d52f431b9cca
Edin Ikanovic <edin@ogtal.dk>
2021-01-26 14:35:42 +0100

    transfer_learn_aws setup

---
+++ transfer_learn_clf/clf_model.py
@@ -0,0 +1,26 @@
+from transformers import AutoTokenizer, AutoModel
+#import torch
+#import torch.nn.functional as F
+import torch.nn as nn
+
+
+#PRE_TRAINED_MODEL_NAME = 'Rostlab/prot_bert_bfd_localization'
+model_checkpoint = 'Maltehb/-l-ctra-danish-electra-small-uncased'
+class TextClassifier(nn.Module):
+    def __init__(self, num_labels):
+        super(TextClassifier, self).__init__()
+        self.pretrained_lm = AutoModel.from_pretrained(model_checkpoint, num_labels=num_labels)
+        self.classifier = nn.Sequential(nn.Dropout(p=0.2),
+                                        nn.Linear(self.pretrained_lm.config.hidden_size, num_labels),
+                                        nn.Tanh())
+        
+    def forward(self, input_ids, attention_mask, labels):
+      output = self.pretrained_model(
+        input_ids=input_ids,
+        attention_mask=attention_mask,
+        labels = labels,
+      )
+      return output
+
+#class RNNModel(nn.Module):
+#    pass
