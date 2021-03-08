daec52d90a7f3fe5fb0d65876a2be55174ceba31
Edin Ikanovic <edin@ogtal.dk>
2021-03-08 13:06:45 +0100

    updated inference script

--- models/electra_med_kontekst/local_inference.py
+++ models/electra_med_kontekst/local_inference.py
@@ -1,16 +1,13 @@
 import pandas as pd
 import torch
-from torch.utils.data import Dataset,RandomSampler,DataLoader
+from torch.utils.data import Dataset,RandomSampler,DataLoader,SequentialSampler
 import numpy as np
 import time
 import argparse
-import logging
 import os
-import sys
 from transformers import AutoTokenizer
 
 from model_def import ElectraWithContextClassifier
-
 
 class DualDatasetInference(Dataset):
     def __init__(self, text, context, ids, tokenizer, max_len):
@@ -64,9 +61,8 @@ class DualDatasetInference(Dataset):
         }
 
 
-
-def get_inference_loader(path,tokenizer,max_len,batch_size,num_workers):
-    dataset = pd.read_csv(path, sep='\t', names = ['ids', 'origin', 'text', 'main_text', 'secondary_text'])
+def get_inference_loader(data_dir, data_file,tokenizer,max_len,batch_size,num_workers):
+    dataset = pd.read_csv(os.path.join(data_dir, data_file), sep='\t', names = ['ids', 'origin', 'text', 'main_text', 'secondary_text'])
 
     dataset['context'] = dataset.origin.astype(str) + ' \n ' + dataset.main_text.astype(str) + ' \n ' + dataset.secondary_text.astype(str)
     dataset['text'] = dataset.text.astype(str)
@@ -79,88 +75,82 @@ def get_inference_loader(path,tokenizer,max_len,batch_size,num_workers):
                     max_len=max_len
                     )
 
-    sampler = RandomSampler(data)
+    #sampler = RandomSampler(data)
+    sampler = SequentialSampler(data)
     dataloader = DataLoader(data,batch_size=batch_size,sampler=sampler,num_workers=num_workers,pin_memory=True)
     return dataloader,data
 
 
 
-def get_model(model_checkpoint)
-    model = ElectraWithContextClassifier(model_checkpoint,2)
-    model.load_state_dict(torch.load('pytorch_model.bin', map_location=torch.device('cpu')))
+def get_model(model_checkpoint, model_weights, num_labels):
+    model = ElectraWithContextClassifier(model_checkpoint,num_labels)
+    model_path = os.path.join(args.model_dir,model_weights)
+    model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
     model.eval()
     return(model)
 
 
+def do_inference(args):
 
-parser = argparse.ArgumentParser()
+    model = get_model(args.model_checkpoint, args.model_weights, args.num_labels)
+    tokenizer = AutoTokenizer.from_pretrained(args.model_checkpoint, use_fast=True)
 
-# Data and model checkpoints directories
-parser.add_argument("--model-checkpoint", type=str, default='Maltehb/-l-ctra-danish-electra-small-cased', help="name of pretrained model from huggingface model hub")
-parser.add_argument("--num-labels", type=int, default=2)
-parser.add_argument("--train", type=str, default='train.csv')
-parser.add_argument("--valid", type=str, default='valid.csv')
-parser.add_argument("--test", type=str, default='test.csv')
-# Hyperparams
-parser.add_argument("--max-len", type=int, default=512)
-parser.add_argument("--batch-size", type=int, default=4)
-parser.add_argument("--test-batch-size", type=int, default=4)
-parser.add_argument("--epochs", type=int, default=2, help="number of epochs to train (default: 2)")
-parser.add_argument("--lr", type=float, default=2e-5)
-parser.add_argument("--weight_decay", type=float, default=0.01)
-parser.add_argument("--seed", type=int, default=43)
-parser.add_argument("--epsilon", type=int, default=1e-8)  
-# Container environment
-parser.add_argument("--model-dir", type=str, default=os.environ["SM_MODEL_DIR"])
-parser.add_argument("--data-dir", type=str, default=os.environ["SM_CHANNEL_DATA"])
-parser.add_argument("--num-gpus", type=int, default=os.environ["SM_NUM_GPUS"])
-parser.add_argument("--num-cpus", type=int, default=os.environ["SM_NUM_CPUS"])
-parser.add_argument("--save-model", type=int, default=1)
-# Log
-parser.add_argument("--verbose", type=bool, default=False)
+    train_loader,train_data = get_inference_loader(args.data_dir,args.input_data,tokenizer,args.max_len,args.batch_size,args.num_cpus)
 
-## RUN
-args = parser.parse_args()
+    t_start = time.time()
 
+    result_ids = []
+    results = {}
 
-model = get_model(args.model_checkpoint)
+    with torch.no_grad():
+        for step, batch in enumerate(train_loader):
+            
+            b_input_ids_text = batch['input_ids_text']
+            b_input_mask_text = batch['attention_mask_text']
+            b_input_ids_context = batch['input_ids_context']
+            b_input_mask_context = batch['attention_mask_context']
+            b_ids = batch['id']
 
-print('got model')
-# tokenizer,dataloader and model
-tokenizer = AutoTokenizer.from_pretrained(args.model_checkpoint, use_fast=True)
+            logits = model(b_input_ids_text, attention_mask_text=b_input_mask_text, input_ids_context=b_input_ids_context, attention_mask_context=b_input_mask_context)
+            y = logits.softmax(1).tolist()
 
-path = '~/projects/medborger-ml/data/dkmed_rep_2019_march_may.csv'
-print('started getting train_loader')
-train_loader,train_data = get_inference_loader(path,tokenizer,args.max_len,args.batch_size,num_workers)
+            for i, idx in enumerate(b_ids):
+                results[idx] = y[i]
 
-device='cpu'
+            if args.verbose:
+                print(f'Finished {(step+1)*4} samples after {round(time.time() - t_start, 2)} seconds. {round((step+1)*4 / (round(time.time() - t_start, 2)), 2)} samples pr. second')
+            else:
+                if step % 100 == 0:
+                    print(f'Finished {(step+1)*4} samples after {round(time.time() - t_start, 2)} seconds. {round((step+1)*4 / (round(time.time() - t_start, 2)), 2)} samples pr. second')
+
+    df = pd.DataFrame.from_dict(results)
+
+    df.to_csv(args.output_data)
 
 
-t_start = time.time()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
 
-result_ids = []
-results = {}
+    # Data and model checkpoints directories
+    parser.add_argument("--model-checkpoint", type=str, default='Maltehb/-l-ctra-danish-electra-small-cased', help="name of pretrained model from huggingface model hub")
+    parser.add_argument("--num-labels", type=int, default=2)
+    parser.add_argument("--model-weights", type=str, default='pytorch_model.bin')
+    
+    # Hyperparams
+    parser.add_argument("--max-len", type=int, default=512)
+    parser.add_argument("--batch-size", type=int, default=4)
 
-with torch.no_grad():
-    for step, batch in enumerate(train_loader):
-        #if args.verbose:
-        print(step)
-        print()
-        b_input_ids_text = batch['input_ids_text'].to(device)
-        b_input_mask_text = batch['attention_mask_text'].to(device)
-        b_input_ids_context = batch['input_ids_context'].to(device)
-        b_input_mask_context = batch['attention_mask_context'].to(device)
-        b_ids = batch['id']
+    # Container environment
+    parser.add_argument("--model-dir", type=str, default=os.environ["SM_MODEL_DIR"])
+    parser.add_argument("--data-dir", type=str, default=os.environ["SM_CHANNEL_DATA"])
+    parser.add_argument("--num-gpus", type=int, default=os.environ["SM_NUM_GPUS"])
+    parser.add_argument("--num-cpus", type=int, default=os.environ["SM_NUM_CPUS"])
+    parser.add_argument("--input-data", type=str, default='dkmed_rep_2019_march_may.csv')
+    parser.add_argument('--output-data', type=str, default='output.csv')
 
-        logits = model(b_input_ids_text, attention_mask_text=b_input_mask_text, input_ids_context=b_input_ids_context, attention_mask_context=b_input_mask_context)
-        y = logits.softmax(1).tolist()
+    # Log
+    parser.add_argument("--verbose", type=bool, default=False)
 
-        for i, idx in enumerate(b_ids):
-            results[idx] = y[i]
-
-        print(f'Finished {(step+1)*4} samples after {round(time.time() - t_start, 2)} seconds')
-
-df = pd.DataFrame.from_dict(results)
-
-df.to_csv('output.txt')
-
+    ## RUN
+    args = parser.parse_args()
+    do_inference(args)
